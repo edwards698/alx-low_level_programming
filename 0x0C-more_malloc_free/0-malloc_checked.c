721fc3169309e0f9d0300666a6fcf34090810144
“edwards698” <edwardphirijr698@gmail.com>
2023-04-13 23:33:29 -0700

    Update

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,40 @@
-#ifndef MAIN_H
-	#define MAIN_H
+#include <stdlib.h>
+		
+#include "main.h"
+		
 
+		
+/**
+		
+ * *malloc_checked - allocates memory using malloc
+		
+ * @b: number of bytes to allocate
+		
+ *
+		
+ * Return: a pointer to the allocated memory
+		
+ */
+		
+void *malloc_checked(unsigned int b)
+		
+{
+		
+	void *ptr;
+		
 
-	#include <stdio.h>
-	#include <stdlib.h>
-	#include <limits.h>
+		
+	ptr = malloc(b);
+		
 
+		
+	if (ptr == NULL)
+		
+		exit(98);
+		
 
-	int _putchar(char c);
-	void *malloc_checked(unsigned int b);
-	char *string_nconcat(char *s1, char *s2, unsigned int n);
-	void *_calloc(unsigned int nmemb, unsigned int size);
-	int *array_range(int min, int max);
-	void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
-	#endif
+		
+	return (ptr);
+		
+}
 
